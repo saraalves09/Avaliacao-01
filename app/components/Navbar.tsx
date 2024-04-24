6e1f414ee558ffda4e4729e1abae21003d6d00b6
sara.vieira <sara.vieira@senff.com.br>
2024-04-24 18:18:05 -0300

    menubar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,18 +1,18 @@
-// Importa as dependências necessárias
+"use client"
 import Image from "next/image"; 
 import Logo from "../../public/assets/Logo.svg"; 
-import Menu from "../../public/assets/Menu.svg";
-import Link from 'next/link'; // Para criar links entre páginas no Next.js
+import MenuIcon from "../../public/assets/Menu.svg"; // Ícone do menu
+import { useState } from 'react'; // Importar useState para controlar o estado do menu
+import Link from 'next/link'; 
 
-// Define os links de navegação
 const navLinks = [
-  { name: "Cadastrar Item", path: '/' }, // Link para a página principal
-  { name: "Lista de Itens", path: '/list/' }, // Link para a página de lista de itens
+  { name: "Cadastrar Item", path: '/' }, 
+  { name: "Lista de Itens", path: '/list/' }, 
 ];
 
-// Define o componente Navbar
 export function Navbar() {
-  //JSX
+  const [isMenuOpen, setIsMenuOpen] = useState(false); // Estado para controlar se o menu está aberto ou fechado
+
   return (
     <nav className="flex w-full items-center justify-between px-[20px] py-[16px] lg:container lg:mx-auto lg:px-20">
       <div className="flex items-center">
@@ -28,7 +28,20 @@ export function Navbar() {
       
       {/* Container para o botão de menu */}
       <div className="flex gap-x-5">
-        <Image src={Menu} alt="Menu Button" className="lg:hidden" />
+        {/* Ícone de menu */}
+        <div onClick={() => setIsMenuOpen(!isMenuOpen)}>
+          <Image src={MenuIcon} alt="Menu" className="cursor-pointer" />
+        </div>
+        {/* Lista suspensa do menu */}
+        {isMenuOpen && (
+          <div className="absolute top-[60px] right-[20px] bg-white border border-gray-200 shadow-lg rounded-md p-2">
+            {navLinks.map((link, index) => (
+              <Link href={link.path} key={index}>
+                <p className="text-[#36485C] font-medium py-1 cursor-pointer">{link.name}</p>
+              </Link>
+            ))}
+          </div>
+        )}
       </div>
     </nav>
   );
